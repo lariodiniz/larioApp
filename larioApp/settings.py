adb15b2b1a4553fc36dcbd06b3b684c21afb68df
lariodiniz <lariodiniz@gmail.com>
2018-12-04 14:00:25 -0200

    Correção do Allowed

--- larioApp/settings.py
+++ larioApp/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '123')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
